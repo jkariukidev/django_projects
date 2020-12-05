c0ecf781045cb8c4bd06f6ef148ae58283f82333
Joseph Kariuki <joehene@gmail.com>
2020-12-05 12:52:42 +0300

    Refactored settings.py file.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1xi6nwyjl319fgkl80b9z#mj=htgpvgdfx&p415%v*o3)xa^sn'
+# Generate own key here.
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
