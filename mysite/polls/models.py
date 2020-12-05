67f02f66375a2d4a361ef39721f30877bd098779
Joseph Kariuki <joehene@gmail.com>
2020-12-05 12:37:22 +0300

    Django polls app from django project tutorials

---
+++ mysite/polls/models.py
@@ -0,0 +1,31 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Question(models.Model):
+    """
+    Question model consisting of a question and a publication date.
+    """
+    def __str__(self):
+        return self.question_text
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    """
+    Choice model consisting of a choice associated with a question.
+    """
+    def __str__(self):
+        return self.choice_text
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
